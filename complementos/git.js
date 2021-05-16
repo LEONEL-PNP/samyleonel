ac4679e7f2ea92d65cdb6e039da86470d98dda3e
LEONEL-PNP <83434309+LEONEL-PNP@users.noreply.github.com>
2021-05-16 17:41:08 -0500

    Update and rename plugins/git.js to complementos/git.js

--- complementos/git.js
+++ complementos/git.js
@@ -1,10 +1,10 @@
 let handler = async m => m.reply(`
  *GIT DE ESTE BOT*
-https://github.com/Samu330/esubot
+Lol
 
 MY GIT:
 
-https://github.com/Samu330
+Lol
 😴                                       
 `.trim()) 
 handler.help = ['git']
