1bbaeb744d84e0b821772d63aa21c2cb8cb015a6
LEONEL-PNP <83434309+LEONEL-PNP@users.noreply.github.com>
2021-05-16 16:27:05 -0500

    Update config.js

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = '🔥𝚂𝚊𝚖 𝚢 𝙿𝚎𝚛𝚛𝚢🥀\n❏ 𝐿𝑎 𝑓𝑒𝑙𝑖𝑐𝑖𝑑𝑎𝑑 𝑛𝑜 𝑒𝑠 𝑒𝑙 𝑜𝑏𝑗𝑒𝑡𝑖𝑣𝑜...\n❏ 𝐄𝐬 𝐞𝐥 𝐜𝐚𝐦𝐢𝐧𝐨\n⚠️Samu330⚠️ '
-global.author = '➬🌤️🏆𝙎𝙖𝙢𝙪𝟯𝟯𝟬🏆🌤️\n↳  🔥Sα͉̳̩͉͔͈̰̠̮̝̠m̗͇̤͎̣̲͍̪̙̜̱ͅ ̬̠̺̟̫ч̖͔̗̲͔͎̲̼͇̠̪͈͖ P̠̠͇̩̪͚̩̥̹͓͚̼er̪̮̺̥̖̣͖̮̦̥ṟ̤͖͙̤̣̥͕͔̲̬ч͙̜̞͈̻̪🥀\n\n\n\n\n      🌺ᵇaͤnͨdͬcͤrͣeͭaͥtͮeͤ🌺\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
+global.packname = '🔥leonel 𝚢 merry🥀\n❏ 𝐿𝑎 𝑓𝑒𝑙𝑖𝑐𝑖𝑑𝑎𝑑 𝑛𝑜 𝑒𝑠 𝑒𝑙 𝑜𝑏𝑗𝑒𝑡𝑖𝑣𝑜...\n❏ 𝐄𝐬 𝐞𝐥 𝐜𝐚𝐦𝐢𝐧𝐨\n⚠️Leonel⚠️ '
+global.author = '➬🌤️🏆LYM🏆🌤️\n↳  🔥Leom̗͇̤͎̣̲͍̪̙̜̱ͅч̖͔̗̲͔͎̲̼͇̠̪͈͖el Y Mer̪̮̺̥̖̣͖̮̦̥ṟ̤͖͙̤̣̥͕͔̲̬ч͙̜̞͈̻̪🥀\n\n\n\n\n      🌺ᵇaͤnͨdͬcͤrͣeͭaͥtͮeͤ🌺\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
 
 global.multiplier = 69 // The higher, The harder levelup
 
